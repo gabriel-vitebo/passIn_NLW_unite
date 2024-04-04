002e3e88c01bc6dfaccce56fb13aa229b3f2d398
gabriel-vitebo <vitebo@outlook.com>
2024-04-04 15:38:17 -0300

    add Fastify Type Provider Zod to API

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,60 @@
 import fastify from 'fastify'
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import { generateSlug } from './utils/generate-slug'
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 const prisma = new PrismaClient({
   log: ['query']
 })
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable()
-  })
-
-  const data = createEventSchema.parse(request.body)
-
-  const event = await prisma.event.create({
-    data: {
-      title: data.title,
-      details: data.details,
-      maximumAttendees: data.maximumAttendees,
-      slug: new Date().toISOString(),
+app
+  .withTypeProvider<ZodTypeProvider>()
+  .post('/events', {
+    schema: {
+      body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+      }),
+      response: {
+        201: z.object({
+          eventId: z.string().uuid()
+        })
+      }
     }
-  })
+  }, async (request, reply) => {
 
-  return reply.status(201).send({ eventId: event.id })
-})
+    const { title, details, maximumAttendees } = request.body
+
+    const slug = generateSlug(title)
+
+    const eventWithSameSlug = await prisma.event.findUnique({
+      where: {
+        slug,
+      }
+    })
+
+    if (eventWithSameSlug !== null) {
+      throw new Error('Another event with same title already exists')
+    }
+
+    const event = await prisma.event.create({
+      data: {
+        title,
+        details,
+        maximumAttendees,
+        slug,
+      }
+    })
+
+    return reply.status(201).send({ eventId: event.id })
+  })
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🟢 HTTP Server running!')
